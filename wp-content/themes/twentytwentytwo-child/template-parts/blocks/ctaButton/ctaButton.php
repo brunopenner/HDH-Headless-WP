96a82600d22e7ba63837fd2b93f5ad6b98054e69
Bruno Penner <brunopenner01@gmail.com>
2024-06-06 14:47:53 +0800

    #24. Assign fields to the CTA block and create block preview

--- wp-content/themes/twentytwentytwo-child/template-parts/blocks/ctaButton/ctaButton.php
+++ wp-content/themes/twentytwentytwo-child/template-parts/blocks/ctaButton/ctaButton.php
@@ -1,2 +1,28 @@
 <?php
-echo "CTA button placholder";
+$blockName = "cta-button";
+$label = get_field('label');
+$destination = get_field('destination');
+$alignment = get_field('alignment');
+
+?>
+<div class="<?php echo $blockName . '-' . $alignment; ?>">
+    <div class="<?php echo $blockName ?>">
+        <?php echo $label ?>
+    </div>
+</div>
+
+<style>
+    .<?php echo $blockName; ?> {
+        padding: 5px 10px;
+        border-radius: 5px;
+        color: white;
+        font-weight: bold;
+        display: inline-block;
+        background: #ec489a;
+    }
+
+    .<?php echo $blockName . '-' . $alignment; ?> {
+        text-align:
+            <?php echo $alignment ?>;
+    }
+</style>
